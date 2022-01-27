769dedc3b31408d979b71cfc55cbdbb41811ac8d
demigorrgon <demigorrgon@gmail.com>
2022-01-27 20:19:04 +0200

    created templated + changed urls

--- tweets/models.py
+++ tweets/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class TweetModel(models.Model):
+    content = models.TextField(max_length=300, blank=True, null=True)
+    image = models.FileField(upload_to="images/", blank=True, null=True)
--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def test_view(request, *args, **kwargs):
+    return render(request, "pages/index.html")
