8fc428b75fa1bd77e33861e1e04c5d0222dd7278
demigorrgon <demigorrgon@gmail.com>
2022-01-29 16:18:46 +0200

    init admin

--- tweets/models.py
+++ tweets/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from django.core.validators import MaxLengthValidator
+from django.contrib.auth import get_user_model
 
 
 class TweetModel(models.Model):
-    content = models.TextField(
-        validators=[MaxLengthValidator], max_length=300, blank=True, null=True
-    )
+    User = get_user_model()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(max_length=300, blank=True, null=True)
     image = models.FileField(upload_to="images/", blank=True, null=True)
 
     class Meta:
