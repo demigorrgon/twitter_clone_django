abd3f04db782eeb0fc60bf31beb777432db9f0d1
demigorrgon <demigorrgon@gmail.com>
2022-02-02 17:50:00 +0200

    coverage-badge test

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # twitter_clone_django
 twitter like app for learning
+[coverage-badge](Z:\projects\django_practice\twitter_clone_django\badge.svg)
--- tweets/tests.py
+++ tweets/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import TweetModel
 
-# Create your tests here.
+
+class TweetsTestCase(TestCase):
+    User = get_user_model()
+
+    def setUp(self):
+        self.User.objects.create(username="somebody", password="somepassword")
+        self.User.objects.create(username="someone", password="somepassword")
+        self.User.objects.create(username="someone_else", password="somepassword")
+
+    def test_user_created(self):
+        user = self.User.objects.get(username="somebody")
+        self.assertEqual(user.username, "somebody")
