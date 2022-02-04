e7c7d01ca8c3a502a5c3c15e8cc5517d899409f3
demigorrgon <demigorrgon@gmail.com>
2022-02-04 15:49:11 +0200

    Revert "endpoints cleanup"
    This reverts commit 8cb87adcce32e51a86b60b261e4a67125cddb27d.

--- tweets/urls.py
+++ tweets/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
 from .views import (
-    home_view,
+    tweet_view,
+    tweet_action_view,
     tweets_list_view,
-
+    tweets_list_json,
+    tweet_create_view,
+    tweet_delete_view,
 )
 
 urlpatterns = [
     path("", tweets_list_view),
-
+    path("tweets/", tweets_list_json),
+    path("tweets/<int:tweet_id>/", tweet_view),
+    path("tweets/create/", tweet_create_view),
+    path("tweets/<int:tweet_id>/delete/", tweet_delete_view),
+    path("tweets/action/", tweet_action_view),
+]
