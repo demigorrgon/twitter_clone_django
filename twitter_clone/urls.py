009ddc7405ddbb56a7761e8a1815476d5c517d6c
demigorrgon <demigorrgon@gmail.com>
2022-02-04 15:27:01 +0200

    reformat of endpoints

--- twitter_clone/urls.py
+++ twitter_clone/urls.py
@@ -14,12 +14,27 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from tweets.views import tweets_list_view
+from django.urls import path, re_path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
+from tweets.views import tweets_list_view, tweets_detail_view, home_view
+from accounts.views import login_view, logout_view, registration_view
 
 
 urlpatterns = [
+    path("", home_view),
     path("admin/", admin.site.urls),
-    path("tweets/", tweets_list_view),
-    path("api/", include("tweets.urls")),
+    path("react/", TemplateView.as_view(template_name="react.html")),
+    path("global/", tweets_list_view),
+    path("login/", login_view),
+    path("logout/", logout_view),
+    path("register/", registration_view),
+    path("<int:tweet_id>", tweets_detail_view),
+    re_path(r"profiles?/", include("profiles.urls")),
+    path("api/tweets/", include("tweets.api.urls")),
+    # re_path(r'api/profiles?/', include('profiles.api.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
