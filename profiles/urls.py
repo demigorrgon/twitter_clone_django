600f5a099fe97381a44a2204fb681c3a9bd8bf27
demigorrgon <demigorrgon@gmail.com>
2022-02-07 12:43:20 +0200

    added endpoint

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,4 +1,10 @@
 from django.urls import path
-from .api.views import profile_tweet_history_view
+from .api.views import profile_tweet_history_view, profile_follow_by_current_user_view
 
-urlpatterns = [path("api/<str:username>", profile_tweet_history_view)]
+"""
+endpoint: api/profiles/
+"""
+urlpatterns = [
+    path("<str:username>", profile_tweet_history_view),
+    path("<str:username>/follow/", profile_follow_by_current_user_view),
+]
