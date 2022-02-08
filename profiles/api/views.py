2fde0792f77323dfd7572e915426ef4bdad891ee
demigorrgon <demigorrgon@gmail.com>
2022-02-08 22:47:21 +0200

    fixed self follow bug

--- profiles/api/views.py
+++ profiles/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404
 from django.contrib.auth import get_user_model
+from django.contrib import messages
 from tweets.models import TweetModel
 from tweets.serializers import TweetSerializer
 from profiles.models import ProfileModel
@@ -21,19 +22,28 @@ def profile_tweet_history_view(request, username, *args, **kwargs):
 
 @api_view(["GET", "POST"])
 def profile_follow_by_current_user_view(request, username, *args, **kwargs):
-    print(username)
     profile_user_object = get_object_or_404(ProfileModel, user__username=username)
-    # print(profile_user_object)
     data = request.data
     if request.method == "POST":
         current_user = request.user
         # xmlhttprequest
         action = data.get("action")
-        if profile_user_object != current_user:
+        if profile_user_object.user.username != current_user.username:
             if action == "follow":
                 profile_user_object.followers.add(current_user)
             elif action == "unfollow":
                 profile_user_object.followers.remove(current_user)
+        else:
+            serializer = PublicProfileSerializer(
+                instance=profile_user_object,
+                context={
+                    "message": "Cannot follow yourself. Unless you really want to"
+                },
+            )
+            # return Response(serializer.context)
+            messages.error(request, serializer._context["message"])
+
+            return Response(serializer._context, status=403)
 
     serializer = PublicProfileSerializer(
         instance=profile_user_object, context={"request": request}
